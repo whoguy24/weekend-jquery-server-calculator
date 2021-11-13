349620fa7425b923fc1bedd07c96cf067ba3560f
whoguy24 <whoguy24@gmail.com>
2021-11-13 12:52:34 -0600

    Wrote clearInputs function.

--- server/public/scripts/calculator.js
+++ server/public/scripts/calculator.js
@@ -1,7 +1,7 @@
 // Initialize jQuery
 $(document).ready(onReady);
 
-// Global Variables
+// Define global variables
 let operation = ""
 
 // Run function on page load
@@ -11,7 +11,7 @@ function onReady () {
     $('#button-subtract').on('click', setOperation);
     $('#button-multiply').on('click', setOperation);
     $('#button-divide').on('click', setOperation);
-    $('#button-clear').on('click', setOperation);
+    $('#button-clear').on('click', clearInputs);
 }
 
 // Sets the current operation type.
@@ -47,4 +47,12 @@ function setOperation () {
         console.log('Click handler failed.');
     }
     
+}
+
+// Clear inputs and remove highlighting from all buttons
+function clearInputs () {
+    console.log('Inputs cleared.');
+    $('.button-operation').removeClass('button-active');
+    $('#input-number-1').val('');
+    $('#input-number-2').val('');
 }
